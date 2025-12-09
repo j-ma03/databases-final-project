ee3011c03eadd3ca219f54c3619fae1985d2c0e0
skateorcry <90222845+skateorcry@users.noreply.github.com>
2025-12-09 13:20:16 -0500

    test queries to check that all tables are present

---
+++ test_query.sql
@@ -0,0 +1,6 @@
+use final_project;
+select * from crimerate;
+select * from countriesgdp;
+select * from presidentialapprovalratings;
+select * from dragonballratings;
+select * from unemploymentcountries;
